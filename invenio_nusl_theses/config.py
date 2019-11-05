9ef9a1fba3835d1743acca44c232ac49980f6e73
semtex <Daniel.Kopecky@techlib.cz>
2019-11-05 10:31:38 +0100

    Permissions done
    Permissions done

--- invenio_nusl_theses/config.py
+++ invenio_nusl_theses/config.py
@@ -14,6 +14,7 @@ from invenio_records_rest.facets import terms_filter
 from invenio_records_rest.utils import allow_all
 
 from invenio_nusl_theses.marshmallow import ThesisRecordSchemaV1, ThesisMetadataSchemaV1
+from invenio_nusl_theses.permissions import thesis_write_permission_factory
 from invenio_nusl_theses.record import PublishedThesisRecord, DraftThesisRecord
 
 THESES_SEARCH_INDEX = 'invenio_nusl_theses-nusl-theses-v1.0.0'
@@ -38,9 +39,12 @@ DRAFT_ENABLED_RECORDS_REST_ENDPOINTS = {
         'draft_record_class': DraftThesisRecord,
         'published_record_class': PublishedThesisRecord,
 
-        'publish_permission_factory': allow_all,
-        'unpublish_permission_factory': allow_all,
-        'edit_permission_factory': allow_all,
+        'publish_permission_factory': thesis_write_permission_factory,
+        'unpublish_permission_factory': thesis_write_permission_factory,
+        'edit_permission_factory': thesis_write_permission_factory,
+        'draft_modify_permission_factory': thesis_write_permission_factory,
+        'draft_read_permission_factory': thesis_write_permission_factory,
+
 
         # 'search_class': DebugACLRecordsSearch,
         # 'indexer_class': CommitingRecordIndexer,
