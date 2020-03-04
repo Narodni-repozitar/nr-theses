717ee36b80cbb40e461d93e65280b3015b7756fd
Daniel Kopecky <kopecky.d@gmail.com>
2020-03-04 18:31:37 +0100

    PEP8

--- invenio_nusl_theses/proxies.py
+++ invenio_nusl_theses/proxies.py
@@ -1,19 +1,19 @@
 #
 # Copyright (c) 2019 UCT Prague.
-# 
-# proxies.py is part of Invenio Explicit ACLs 
+#
+# proxies.py is part of Invenio Explicit ACLs
 # (see https://github.com/oarepo/invenio-explicit-acls).
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -29,4 +29,5 @@ from flask import current_app
 from werkzeug.local import LocalProxy
 
 nusl_theses = LocalProxy(
-    lambda: current_app.extensions['invenio-nusl-theses'])  # type: invenio_nusl_theses.api.ThesisAPI
+    lambda: current_app.extensions[
+        'invenio-nusl-theses'])  # type: invenio_nusl_theses.api.ThesisAPI
